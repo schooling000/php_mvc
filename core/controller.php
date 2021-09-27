5b0c03898464b7b1983355f7392c1ffc781f4795
unknown <schooling010@gmail.com>
2021-09-27 14:00:57 +0700

    UPDATE_27_09_2021

---
+++ core/controller.php
@@ -0,0 +1,13 @@
+<?php 
+class Controller{
+    private $view;
+    private $model;
+
+    public function __construct()
+    {
+        
+    }
+
+    
+
+}
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    # define root_path;
+    define('ROOT_PATH',__DIR__);
