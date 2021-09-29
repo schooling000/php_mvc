8585c36ea3088bd492b2e771cc2bdf66ca639c7e
unknown <schooling010@gmail.com>
2021-09-29 16:59:52 +0700

    UPDATE_29_09/2021

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-    # define root_path;
-    define('ROOT_PATH',__DIR__);
+# define root_path;
+define('ROOT_PATH', __DIR__);
+?>
+
+<form class="form-horizontal" name="contact_form" id="contact_form" method="post" action="">
+<input type="hidden" name="mode" value="login" >
+<fieldset>
+<div class="form-group">
+<label class="col-lg-2 control-label" for="username"><span class="required">*</span>Username:</label>
+<div class="col-lg-6">
+<input type="text" value="" placeholder="User Name" id="username" class="form-control" name="username" required="" >
+</div>
+</div>
+<div class="form-group">
+<label class="col-lg-2 control-label" for="user_password"><span class="required">*</span>Password:</label>
+<div class="col-lg-6">
+<input type="password" value="" placeholder="Password" id="user_password" class="form-control" name="user_password" required="" >
+</div>
+</div>
+<div class="form-group">
+<div class="col-lg-6 col-lg-offset-2">
+<button class="btn btn-primary" type="submit">Submit</button> 
+</div>
+</div>
+</fieldset>
+</form>
