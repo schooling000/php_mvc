78ca8099d58545045e1792a24d997acb4650327b
schooling000 <schooling010@gmail.com>
2025-06-01 10:03:43 +0700

    update

--- app/core/validate/validate.php
+++ app/core/validate/validate.php
@@ -11,17 +11,23 @@ namespace app\core\validate {
 
         public function addField($name, $value, $message = ""): Validate
         {
-            $this->fields[$name]=$value;
+            $this->fields[$name]['value'] = $value;
+            $this->fields[$name]['message'] = "";
+            $this->fields[$name]['hasError'] = false;
             return $this;
         }
 
-        public function textField(): void {}
+        public function textField(string $name, bool $required = true, int $min = 1, int $max = 244): void {}
         public function numberField(): void {}
         public function phoneField(): void {}
         public function emailField(): void {}
 
-        public function fieldIsError() : bool {}
-        public function getMessageFieldError() : string {}
-        public function hasFieldError() : bool {}
+        public function fieldIsError(): bool {}
+        public function getMessageFieldError(): string {}
+        public function hasFieldError(): bool {}
+        public function __destruct()
+        {
+            unset($this->fields);
+        }
     }
 }
